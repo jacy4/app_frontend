f967b45f5336c99bb95b70ba2396b0fbb88a3951
Luís Cunha <estgv14649@alunos.estgv.ipv.pt>
2024-07-22 23:34:30 +0100

    22072024

--- src/views/EVENTOS/eventos.js
+++ src/views/EVENTOS/eventos.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './eventos.css';
 
-const EventosView = () => {
-  return (
-    <div className="div_princ_eventos">
-      <h1 className="title2">Lista de Eventos</h1>
-      
-      <p>Ainda está por fazer !!!</p>
-    </div>
-  );
+import axios from 'axios';
+
+const EventosView = ({ centroId }) => {
+    const [eventos, setEventos] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        axios.get(`https://backend-teste-q43r.onrender.com/eventos/listarEventos/${centroId}`)
+            .then(response => {
+                setEventos(response.data);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error('Erro ao buscar eventos:', error);
+                setLoading(false);
+            });
+    }, [centroId]);
+
+    if (loading) {
+        return <p>Carregando...</p>;
+    }
+
+    return (
+        <div>
+            {eventos.length === 0 ? (
+                <p>Nenhuma EVENTO encontrada</p>
+            ) : (
+                eventos.map(evento => (
+                    <div key={evento.id}>
+                        <h2>{evento.nome}</h2>
+                        <p>{evento.descricao}</p>
+                        <p>Topico: {evento.topico.nome}</p>
+                        {/* Outros detalhes do evento */}
+                    </div>
+                ))
+            )}
+        </div>
+    );
 };
 
 export default EventosView;
